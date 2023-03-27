b4e1ed84dc31e2ab9cfa465cba0467dfb74903a1
Mateusz Sikorski <mati.sikor07@gmail.com>
2023-03-27 14:07:56 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
 
-echo "⟳ Uruchamianie PHP-FPM..."
+echo "⟳  Uruchamianie PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
-echo "⟳ Uruchamianie Nginx..."
-echo "✓ Serwer wystartował"
+echo "⟳  Uruchamianie Nginx..."
+echo "✓  Serwer wystartował"
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
