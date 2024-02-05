493f2adf268829baf1a54e029938e5d8ef1abbb7
Mateusz Sikorski <mati.sikor07@gmail.com>
2024-02-05 20:10:56 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,15 @@ cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+
+rm -rf /home/container/tmp/*
+
+echo "⟳  Uruchamianie PHP-FPM..."
+/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+
+echo "⟳  Uruchamianie Nginx..."
+echo "✓  Serwer wystartował"
+/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
+
 # Run the Server
 ${MODIFIED_STARTUP}
