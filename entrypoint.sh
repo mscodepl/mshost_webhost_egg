4bae26576a9e9073b20ec7ca80b93d85f39d2da5
Mateusz Sikorski <mati.sikor07@gmail.com>
2024-02-07 19:31:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo "Startowanie serwera..."
 
+./run.sh
+
 # Run the Server
 ${MODIFIED_STARTUP}
